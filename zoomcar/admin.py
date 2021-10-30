783f28d67fa1d4d0ac797280de88eeef53c90db3
nquintana <nicolasqm@uma.es>
2021-10-30 12:05:47 +0200

    test 2

--- zoomcar/admin.py
+++ zoomcar/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from zoomcar.models import Ubicacion, Usuario
+from zoomcar.models import Trayecto, Ubicacion, Usuario
 
 # Register your models here.
 
 admin.site.register(Usuario)
 admin.site.register(Ubicacion)
+admin.site.register(Trayecto)
