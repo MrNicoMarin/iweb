63cc5d16a8b03406b48da6503b3b30fc0c3ed941
MrNicoMarin <nicolasm008@gmail.com>
2021-10-31 14:08:36 +0100

    Admin

--- zoomcar/admin.py
+++ zoomcar/admin.py
@@ -4,3 +4,5 @@ from zoomcar.models import Comentario, Trayecto, Ubicacion, Usuario
 # Register your models here.
 
 admin.site.register(Usuario)
+admin.site.register(Ubicacion)
+admin.site.register(Trayecto)
